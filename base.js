e7d6e8476678f63437ac21d7592d938c1a794fa7
Ninu1234 <55695424+Ninu1234@users.noreply.github.com>
2021-09-19 10:57:59 +0530

    Add files via upload

---
+++ base.js
@@ -0,0 +1,13 @@
+// Registering component in box-component.js
+AFRAME.registerComponent("base", {
+    schema: {
+      radius: { type: "number", default: 150 },
+      height: {type: "number", default: 3}
+    },
+  
+    init: function () {
+     this.el.setAttribute("geometery", {primitive:'cylinder', radius:this.data.radius, height:this.data.height});
+     this.el.setAttribute("material", {color:'#1769ab'});
+    }
+  });
+  
